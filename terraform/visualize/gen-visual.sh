6733b508d688d7fff6aac441554c66df5791662e
Eleftherios Myriounis <el.myrioun@gmail.com>
2023-08-29 16:37:06 +0000

    vuejs deployment & tf visualize

---
+++ terraform/visualize/gen-visual.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+terraform state pull > ./tf.state
+
+rm -rf ./inframap
+git clone https://github.com/cycloidio/inframap
+cd inframap
+go mod download
+make build
+cd ..
+
+terraform graph -type=plan | dot -Tpng > ./tf-graph.default.png
+./inframap generate --connections=false ./tf.state | dot -Tpng > ./tf-graph.lite.png
+./inframap generate ./tf.state --raw | dot -Tpng > ./tf-graph.full.png
+
+rm ./tf.state
+rm -rf ./inframap
+
+echo "Visualization generated successfully"
