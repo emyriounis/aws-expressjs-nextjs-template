8108e53b40f941f91fdeaccfe2cf5c544c6c9514
Eleftherios Myriounis <el.myrioun@gmail.com>
2026-03-16 22:14:24 +0200

    update & cleanup

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,9 +4,7 @@ locals {
   env_prefix = var.enviroment == "prod" ? "" : "${var.enviroment}."
   subdomain  = "${local.env_prefix}${var.name}.${var.base_domain}"
 
-  expressjs_domain  = "expressjs.${local.subdomain}"
-  nextjs_v12_domain = "nextjs_v12.${local.subdomain}"
-  nextjs_domain     = "nextjs.${local.subdomain}"
-  reactjs_domain    = "reactjs.${local.subdomain}"
-  vuejs_domain      = "vuejs.${local.subdomain}"
+  expressjs_domain = "expressjs.${local.subdomain}"
+  nextjs_domain    = "nextjs.${local.subdomain}"
+  reactjs_domain   = "reactjs.${local.subdomain}"
 }
---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "expressjs_domain" {
+  value = local.expressjs_domain
+}
+
+output "nextjs_domain" {
+  value = local.nextjs_domain
+}
+
+output "reactjs_domain" {
+  value = local.reactjs_domain
+}
