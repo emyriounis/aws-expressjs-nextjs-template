b93ae8d06fbc418ee5a6ec5305a75ee570d63bdb
Eleftherios Myriounis <el.myrioun@gmail.com>
2026-05-04 00:55:33 +0300

    feat: add password policy to Cognito user pool and restrict explicit auth flows

--- expressjs/terraform/cognito.tf
+++ expressjs/terraform/cognito.tf
@@ -14,13 +14,22 @@ resource "aws_cognito_user_pool" "this" {
   admin_create_user_config {
     allow_admin_create_user_only = true
   }
+
+  password_policy {
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = false
+    require_uppercase                = true
+    temporary_password_validity_days = 7
+  }
 }
 
 resource "aws_cognito_user_pool_client" "this" {
   name         = "${local.resource_prefix}-client"
   user_pool_id = aws_cognito_user_pool.this.id
 
-  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH"]
+  explicit_auth_flows           = ["ALLOW_USER_SRP_AUTH"]
   prevent_user_existence_errors = "ENABLED"
 
   access_token_validity  = 8   #hours
