8638e1138f5a1a058e4c8492667f7de4c2bfe406
Eleftherios Myriounis <el.myrioun@gmail.com>
2026-05-03 22:58:15 +0300

    feat: update Cognito auth flows and adjust CI/CD workflow dependencies

--- expressjs/terraform/cognito.tf
+++ expressjs/terraform/cognito.tf
@@ -20,7 +20,7 @@ resource "aws_cognito_user_pool_client" "this" {
   name         = "${local.resource_prefix}-client"
   user_pool_id = aws_cognito_user_pool.this.id
 
-  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH"]
+  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
   prevent_user_existence_errors = "ENABLED"
 
   access_token_validity  = 8   #hours
