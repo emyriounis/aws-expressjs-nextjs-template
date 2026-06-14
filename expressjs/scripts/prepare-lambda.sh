bdc98d8ac7ff10f3149bfbda17f5bc032f810284
Eleftherios Myriounis <el.myrioun@gmail.com>
2026-06-14 17:24:52 +0300

    chore: optimize Lambda deployment size

--- expressjs/scripts/prepare-lambda.sh
+++ expressjs/scripts/prepare-lambda.sh
@@ -31,5 +31,24 @@ rm -rf nodejs/node_modules/@prisma/engines/*darwin*
 rm -rf nodejs/node_modules/prisma/*darwin*
 rm -rf nodejs/node_modules/.prisma/client/*darwin*
 
+# Remove native query engine binaries to save size (the Data API adapter uses WASM/JS engine in production)
+rm -rf nodejs/node_modules/@prisma/engines/libquery_engine*
+rm -rf nodejs/node_modules/prisma/libquery_engine*
+rm -rf nodejs/node_modules/.prisma/client/libquery_engine*
+
+# Remove peerOptional typescript package to save 23MB
+rm -rf nodejs/node_modules/typescript
+
+# Remove non-postgresql WASM files to save ~39MB
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_engine_bg.cockroachdb*
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_engine_bg.mysql*
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_engine_bg.sqlite*
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_engine_bg.sqlserver*
+
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_compiler_bg.cockroachdb*
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_compiler_bg.mysql*
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_compiler_bg.sqlite*
+rm -rf nodejs/node_modules/@prisma/client/runtime/query_compiler_bg.sqlserver*
+
 zip -rq layer.zip nodejs
 rm -r nodejs
